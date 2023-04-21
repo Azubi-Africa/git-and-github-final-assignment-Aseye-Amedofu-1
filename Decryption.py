cb9dba6387a3e39112250f28f5f64ca51c7f8954
Samuel Aseye Amedofu <aseye.amedofu@gmail.com>
2023-04-21 10:09:46 +0000

    Git Tracking

---
+++ Decryption.py
@@ -0,0 +1,16 @@
+cipher_text = "XYZABCDEFGHIJKLMNOPQRSTUVW"
+    
+    
+text = input("Enter cipher message: ").upper()
+#s defines the shift count
+S = int(input("Choose the shift code: "))
+
+decrypted = str()
+
+# transverse the cipher text
+for i in text:
+    updated_position = (cipher_text.find(i)+S) % 26
+    decrypted += cipher_text[updated_position]
+
+print(decrypted)
+
